19c2034b6a0a3b0296edfe53a796e6281b438cce
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-09-14 16:27:59 -0400

    Bid auction: Add auction bidding logic with winner calculation

---
+++ Day9_Bid auction.py
@@ -0,0 +1,25 @@
+bids = {}
+
+while True:
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: "))
+
+    bids[name] = bid
+    other_bidder = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
+    print("\n" * 20)
+
+    if other_bidder != "yes":
+        break
+
+print(f"Bid auctions list: \n{bids}")
+
+highest_bid = 0
+winner = ""
+
+for person in bids:
+    if bids[person] > highest_bid:
+        highest_bid = bids[person]
+        winner = person
+
+print(f"The winner is {winner} with a bid of ${highest_bid}")
+    
