a48bcb21378cdf294808c702be47d54812ee1001
Anusha Dunaboyina <anusha.dunaboyina@gmail.com>
2025-09-16 13:27:44 -0400

    Implemented prime number checker logic

---
+++ 11. Prime_number_checker.py
@@ -0,0 +1,19 @@
+while True:
+    def is_prime(num):
+        if num <= 1 :
+            return False
+
+        # Check for divisibility from 2 up to the square root of num
+
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:            # If divisible by any number other than 1 and itself, it's not prime
+
+                return False
+        else:
+            return True
+
+    print(is_prime(int(input("Enter a number: "))))
+
+    another_number = input("Continue 'y' or 'n': ").lower()
+    if another_number != "y":
+        break
